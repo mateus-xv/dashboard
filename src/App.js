a1defda5777c98a18da034bc8f1248aa8451e67c
Mateus Felipe <mat.fel.oli@gmail.com>
2021-06-23 23:38:20 -0300

    Add Aside Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+//React Modules
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+//Redux
+import store from './redux/store/store';
+
+//Components
+import Aside from './components/Aside';
+
+//Stylesheets
+import './scss/base/index.scss'
+import './scss/layout/index.scss'
+import './scss/module/index.scss'
+import './scss/state/index.scss'
+import '@fortawesome/fontawesome-free/css/all.css'
+
 function App(){
     return(
-        <Provider>
+        <Provider store={store}>
             <BrowserRouter>
+                <Aside />
             </BrowserRouter>
         </Provider>
     )
---
+++ src/components/Aside.js
@@ -0,0 +1,22 @@
+//React Modules
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+
+const Aside = () => {
+    return(
+        <aside>
+            <div className='logo'>
+                <h1 className='logo__name'>Dashboard</h1>
+            </div>
+            <nav className='aside-nav'>
+                <NavLink className='aside-nav__link' to="/home"><i className="fas fa-angle-right"></i>Home</NavLink>
+                <NavLink className='aside-nav__link' to="/feed"><i className="fas fa-angle-right"></i>Feed</NavLink>
+                <NavLink className='aside-nav__link' to="/notes"><i className="fas fa-angle-right"></i>Notes</NavLink>
+                <NavLink className='aside-nav__link' to="/profile"><i className="fas fa-angle-right"></i>Profile</NavLink>
+                <NavLink className='aside-nav__link' to="/saved"><i className="fas fa-angle-right"></i>Saved</NavLink>
+            </nav>
+        </aside>
+    )
+};
+
+export default Aside;
