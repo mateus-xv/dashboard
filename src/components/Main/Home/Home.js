4adfc547e0d1ac066e30d4d09b843815cb0fb1aa
Mateus Felipe <mat.fel.oli@gmail.com>
2021-07-12 21:14:36 -0300

    Add Topics Component

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -3,11 +3,13 @@ import React from 'react';
 
 //Components
 import News from './News';
+import Topics from '../shared/Topics/Topics';
 
 const Home = () => {
 	return(
 		<div className="cards-wrapper">
 			<News/>
+			<Topics/>
 		</div>	
 	);
 }
---
+++ src/components/Main/shared/Topics/Topics.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Topics = () => {
+	return(
+		<section className="topics">
+				<h2 className="topics__title">Lorem Ipsum</h2>
+				<ul className="topics__container">
+					<li className="topics__single">Lorem Ipsum</li>	
+					<li className="topics__single">Lorem Ipsum</li>	
+					<li className="topics__single">Lorem Ipsum</li>	
+					<li className="topics__single">Lorem Ipsum</li>	
+					<li className="topics__single">Lorem Ipsum</li>	
+					<li className="topics__single">Lorem Ipsum</li>	
+					<li className="topics__single">Lorem Ipsum</li>	
+					<li className="topics__single">Lorem Ipsum</li>	
+					<li className="topics__single">Lorem Ipsum</li>	
+				</ul>
+		</section>
+	)
+};
+
+export default Topics;
