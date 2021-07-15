c314daf63d6d89a8d75de1eeed8ae316d1fbbc8a
Mateus Felipe <mat.fel.oli@gmail.com>
2021-07-14 22:02:27 -0300

    Add Support Component

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -4,12 +4,14 @@ import React from 'react';
 //Components
 import News from './News';
 import Topics from '../shared/Topics/Topics';
+import Support from './Support';
 
 const Home = () => {
 	return(
 		<div className="cards-wrapper">
 			<News/>
 			<Topics/>
+			<Support/>
 		</div>	
 	);
 }
---
+++ src/components/Main/Home/Support.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Support = () => {
+	return(
+		<section className="support">
+			<h2 className="support__title">Lorem Ipsum</h2>
+			<form action="#" className="support__form" >
+				<textarea className="support__message" name="message" id="message"></textarea>
+				<button className="support__submit"><i className="fas fa-angle-right"></i></button>
+			</form>
+		</section>
+	)
+};
+
+export default Support;
