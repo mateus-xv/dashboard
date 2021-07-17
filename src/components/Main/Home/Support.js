80e64df327cdb53bb6c909d62da7d82d2ba0acd1
Mateus Felipe <mat.fel.oli@gmail.com>
2021-07-16 23:04:18 -0300

    Add Submit Handling And Success Feedback

--- src/components/Main/Home/Support.js
+++ src/components/Main/Home/Support.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 const Support = () => {
+	//Activates the success div
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const success = document.querySelector(".form-success");
+		success.classList.add("form-success--visible");
+	};
+
 	return(
 		<section className="support">
 			<h2 className="support__title">Lorem Ipsum</h2>
-			<form action="#" className="support__form" >
+			<form className="support__form"  onSubmit={handleSubmit}>
 				<textarea className="support__message" name="message" id="message"></textarea>
 				<button className="support__submit"><i className="fas fa-angle-right"></i></button>
+				<div className="form-success">
+					<p className="form-success__message">Sent!</p>
+					<button className="form-success__button"><i className="fas fa-check"></i></button>
+				</div>
 			</form>
 		</section>
 	)
