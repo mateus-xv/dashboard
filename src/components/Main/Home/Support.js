c363065d37f24bd5770ae7fa91fa24c81b16b2a9
Mateus Felipe <mat.fel.oli@gmail.com>
2021-07-20 22:42:36 -0300

    Add Success Feedback Handling

--- src/components/Main/Home/Support.js
+++ src/components/Main/Home/Support.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Support = () => {
-	//Activates the success div
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		const success = document.querySelector(".form-success");
+	//Activates the success div received as parameter when form is submitted 
+	const handleSubmit = (e, success) => {
 		success.classList.add("form-success--visible");
+		e.preventDefault();
 	};
+
+	//Removes the success feedback when the success div is clicked
+	const handleSuccess = (e, success) => {
+		success.classList.remove("form-success--visible");
+	};
+
+	useEffect(() => {
+		const form = document.querySelector(".support__form"),
+			success = document.querySelector(".form-success"),
+			successDiv = document.querySelector(".form-success, .form-success__message");
+		form.addEventListener("submit", (e) => handleSubmit(e, success));
+		successDiv.addEventListener("click", (e) => handleSuccess(e, success));
+	}, []);
 
 	return(
 		<section className="support">
 			<h2 className="support__title">Lorem Ipsum</h2>
-			<form className="support__form"  onSubmit={handleSubmit}>
+			<form className="support__form">
 				<textarea className="support__message" name="message" id="message"></textarea>
 				<button className="support__submit"><i className="fas fa-angle-right"></i></button>
 				<div className="form-success">
-					<p className="form-success__message">Sent!</p>
-					<button className="form-success__button"><i className="fas fa-check"></i></button>
+					<p className="form-success__message">Sent!<br/><i className="fas fa-check"></i></p>
 				</div>
 			</form>
 		</section>
