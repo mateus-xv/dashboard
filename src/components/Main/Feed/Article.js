e5b80a6ab894596cf0c6d518507d15735cfad9d8
Mateus Felipe <mat.fel.oli@gmail.com>
2021-07-26 21:00:51 -0300

    Add Article Component

---
+++ src/components/Main/Feed/Article.js
@@ -0,0 +1,22 @@
+//React Modules
+import React from 'react';
+
+const Article = () => {
+	return(
+		<section className="article">
+			<h2 className="article__title">Lorem Ipsum</h2>
+			<p className="article__body">
+			Labore consequat nulla ut elit anim cupidatat enim cillum elit ad dolor. 
+			Pariatur fugiat dolore in culpa culpa sed sint excepteur in esse enim nisi sint ad anim magna exercitation 
+			sint reprehenderit pariatur irure laboris pariatur commodo irure sed irure exercitation ad laborum aute 
+			mollit reprehenderit cupidatat pariatur.
+			</p>
+			<div className="article__actions">
+				<button className="article__save"><i className="far fa-bookmark"></i></button>
+				<button className="article__see-more">See More <i className="fas fa-arrow-circle-right"></i></button>
+			</div>
+		</section>
+	)
+}
+
+export default Article;
--- src/components/Main/Feed/Feed.js
+++ src/components/Main/Feed/Feed.js
@@ -1,9 +1,18 @@
 //React Modules
 import React from 'react';
 
+//Components
+import Article from './Article'
+
 const Feed = () => {
 	return(
-		<div></div>
+		<React.Fragment>	
+			<div className="cards-wrapper">
+				<Article/>
+			</div>
+			<div className="column-wrapper">
+			</div>
+		</React.Fragment>
 	);
 }
 
