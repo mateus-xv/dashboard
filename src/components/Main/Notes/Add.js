e11b20a9a44487041c6378e708ad8c30f2c71eac
Mateus Felipe <mat.fel.oli@gmail.com>
2021-07-30 20:59:53 -0300

    Add Add Component

---
+++ src/components/Main/Notes/Add.js
@@ -0,0 +1,10 @@
+//React Modules
+import React from 'react';
+
+const Add = () => {
+	return(
+		<button className="add"><i className="fas fa-plus"></i></button>
+	)
+}
+
+export default Add;
--- src/components/Main/Notes/Notes.js
+++ src/components/Main/Notes/Notes.js
@@ -3,11 +3,13 @@ import React from 'react';
 
 //Components
 import Note from './Note';
+import Add from './Add';
 
 const Notes = () => {
 	return(
 		<React.Fragment>
 			<Note/>
+			<Add/>
 		</React.Fragment>
 	);
 }
