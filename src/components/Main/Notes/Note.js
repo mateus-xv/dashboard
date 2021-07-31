8300735f829866a53c89bd8cdcfc6c7313b0a944
Mateus Felipe <mat.fel.oli@gmail.com>
2021-07-30 21:21:19 -0300

    Add Note Modal

--- src/components/Main/Notes/Note.js
+++ src/components/Main/Notes/Note.js
@@ -1,5 +1,5 @@
 //React Modules
-import React from "react";
+import React, {useEffect} from "react";
 
 const Note = () => {
 	return(
@@ -11,6 +11,20 @@ const Note = () => {
 					minim mollit eiusmod exercitation ut sunt do ad eu eu excepteur esse 
 					duis aute fugiat amet ut ad.
 				</p>
+				<div className="note__modal">
+					<div className="note__body">
+						<h2 className="note__title">Lorem Ipsum</h2>
+						<p className="note__content">
+							Lorem ipsum ullamco consectetur eu deserunt anim adipisicing ex officia 
+							minim mollit eiusmod exercitation ut sunt do ad eu eu excepteur esse 
+							duis aute fugiat amet ut ad.
+							<br/>
+							Excepteur aute exercitation est minim. Elit minim adipisicing occaecat quis.
+							Commodo quis. Eiusmod excepteur ad aute eu. Esse in enim mollit fugiat in ut 
+							cupidatat aute sed adipisicing in proident dolore in pariatur eu minim.
+						</p>
+					</div>
+				</div>
 			</li>
 		</ul>
 	);
