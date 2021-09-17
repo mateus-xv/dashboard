8f9bf9bb38157895cc2e830ef48f549610368582
Mateus Felipe <mat.fel.oli@gmail.com>
2021-09-16 21:33:06 -0300

    Add Redirection In Topics

--- src/components/Main/shared/Topics/Topics.js
+++ src/components/Main/shared/Topics/Topics.js
@@ -1,116 +1,128 @@
 //React Modules
-import React from "react";
+import React, { useEffect } from "react";
 
 //API Async Function
 import getNews from "utils/getNews";
 
 const Topics = () => {
-	//Returns the className based on the hash
-	const urlCheck = () => {
-		const path = window.location.hash;
-		if (path === "#/feed") return "topics--feed";
+    //Returns the className based on the hash
+    const urlCheckClass = () => {
+        const path = window.location.hash;
+        return path === "#/feed" ? "topics--feed" : "topics";
+    };
 
-		return "topics";
-	};
+    //Redirects to feed page if clicked on home
+    const urlCheckRedirect = () => {
+        const path = window.location.hash;
+        if (path === "#/") window.location.hash = "#/feed";
+    };
 
-	return (
-		<section className={urlCheck()}>
-			<h2 className="topics__title">Topics</h2>
-			<ul className="topics__container">
-				<li onClick={() => getNews("arts")} className="topics__single">
-					<button className="topics__button">Arts</button>
-				</li>
-				<li className="topics__single">
-					<button
-						onClick={() => getNews("automobiles")}
-						className="topics__button"
-					>
-						Automobiles
-					</button>
-				</li>
-				<li className="topics__single">
-					<button
-						onClick={() => getNews("business")}
-						className="topics__button"
-					>
-						Business
-					</button>
-				</li>
-				<li className="topics__single">
-					<button
-						onClick={() => getNews("food")}
-						className="topics__button"
-					>
-						Food
-					</button>
-				</li>
-				<li className="topics__single">
-					<button
-						onClick={() => getNews("health")}
-						className="topics__button"
-					>
-						Health
-					</button>
-				</li>
-				<li className="topics__single">
-					<button
-						onClick={() => getNews("movies")}
-						className="topics__button"
-					>
-						Movies
-					</button>
-				</li>
-				<li className="topics__single">
-					<button
-						onClick={() => getNews("politics")}
-						className="topics__button"
-					>
-						Politics
-					</button>
-				</li>
-				<li className="topics__single">
-					<button
-						onClick={() => getNews("science")}
-						className="topics__button"
-					>
-						Science
-					</button>
-				</li>
-				<li className="topics__single">
-					<button
-						onClick={() => getNews("sports")}
-						className="topics__button"
-					>
-						Sports
-					</button>
-				</li>
-				<li className="topics__single">
-					<button
-						onClick={() => getNews("technology")}
-						className="topics__button"
-					>
-						Technology
-					</button>
-				</li>
-				<li className="topics__single">
-					<button
-						onClick={() => getNews("travel")}
-						className="topics__button"
-					>
-						Travel
-					</button>
-				</li>
-				<li className="topics__single">
-					<button
-						onClick={() => getNews("world")}
-						className="topics__button"
-					>
-						World
-					</button>
-				</li>
-			</ul>
-		</section>
-	);
+    useEffect(() => {
+        document.querySelectorAll(".topics__button").forEach((button) => {
+            button.addEventListener("click", () => {
+                urlCheckRedirect();
+            });
+        });
+    }, []);
+
+    return (
+        <section className={urlCheckClass()}>
+            <h2 className="topics__title">Topics</h2>
+            <ul className="topics__container">
+                <li onClick={() => getNews("arts")} className="topics__single">
+                    <button className="topics__button">Arts</button>
+                </li>
+                <li className="topics__single">
+                    <button
+                        onClick={() => getNews("automobiles")}
+                        className="topics__button"
+                    >
+                        Automobiles
+                    </button>
+                </li>
+                <li className="topics__single">
+                    <button
+                        onClick={() => getNews("business")}
+                        className="topics__button"
+                    >
+                        Business
+                    </button>
+                </li>
+                <li className="topics__single">
+                    <button
+                        onClick={() => getNews("food")}
+                        className="topics__button"
+                    >
+                        Food
+                    </button>
+                </li>
+                <li className="topics__single">
+                    <button
+                        onClick={() => getNews("health")}
+                        className="topics__button"
+                    >
+                        Health
+                    </button>
+                </li>
+                <li className="topics__single">
+                    <button
+                        onClick={() => getNews("movies")}
+                        className="topics__button"
+                    >
+                        Movies
+                    </button>
+                </li>
+                <li className="topics__single">
+                    <button
+                        onClick={() => getNews("politics")}
+                        className="topics__button"
+                    >
+                        Politics
+                    </button>
+                </li>
+                <li className="topics__single">
+                    <button
+                        onClick={() => getNews("science")}
+                        className="topics__button"
+                    >
+                        Science
+                    </button>
+                </li>
+                <li className="topics__single">
+                    <button
+                        onClick={() => getNews("sports")}
+                        className="topics__button"
+                    >
+                        Sports
+                    </button>
+                </li>
+                <li className="topics__single">
+                    <button
+                        onClick={() => getNews("technology")}
+                        className="topics__button"
+                    >
+                        Technology
+                    </button>
+                </li>
+                <li className="topics__single">
+                    <button
+                        onClick={() => getNews("travel")}
+                        className="topics__button"
+                    >
+                        Travel
+                    </button>
+                </li>
+                <li className="topics__single">
+                    <button
+                        onClick={() => getNews("world")}
+                        className="topics__button"
+                    >
+                        World
+                    </button>
+                </li>
+            </ul>
+        </section>
+    );
 };
 
 export default Topics;
