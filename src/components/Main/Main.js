efadf1ddaa6cfa9d670fc65b08974d2a76f2b319
Mateus Felipe <mat.fel.oli@gmail.com>
2021-09-28 21:54:34 -0300

    Add Router Animations

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 //React Modules
 import React from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import {Switch, Route, Redirect, useLocation} from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 //Components
 import Home from './Home/Home'
@@ -9,17 +10,24 @@ import Notes from './Notes/Notes'
 import Profile from './Profile/Profile'
 import Saved from './Saved/Saved'
 
+import 'scss/state/_animations.scss';
+
 const Main = () => {
+    const location = useLocation();
 	return(
 		<main className="clearfix">
-			<Switch>
-				<Route path="/feed" component={Feed}/>
-				<Route path="/profile" component={Profile}/>
-				<Route path="/saved" component={Saved}/>
-				<Route path="/notes" component={Notes}/>
-				<Route path="/" component={Home}/>
-				<Redirect to="/"/>
-			</Switch>			
+            <TransitionGroup>
+                <CSSTransition key={location.pathname} classNames='page' timeout={300}>
+                    <Switch location={location}>
+                        <Route path="/feed" component={Feed}/>
+                        <Route path="/profile" component={Profile}/>
+                        <Route path="/saved" component={Saved}/>
+                        <Route path="/notes" component={Notes}/>
+                        <Route path="/" component={Home}/>
+                        <Redirect to="/"/>
+                    </Switch>			
+                </CSSTransition>
+            </TransitionGroup>
 		</main>
 	);
 };
