240cfea642d68a3774a71552729310829be4785c
Mateus Felipe <mat.fel.oli@gmail.com>
2021-10-28 20:32:02 -0300

    Add Nav Selected Link CSS

--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -1,22 +1,47 @@
 //React Modules
-import React from 'react';
-import { NavLink } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 const Aside = () => {
-    return(
+    let [currentPath, setCurrentPath] = useState(window.location.hash);
+    const urlCheck = (path = "#/") => {
+        if (currentPath === path) {
+            return "fas fa-angle-double-right";
+        } else {
+            return "fas fa-angle-right";
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("hashchange", () =>
+            setCurrentPath(window.location.hash)
+        );
+    }, []);
+
+    return (
         <aside>
-            <div className='logo'>
-                <h1 className='logo__name'>Dashboard</h1>
+            <div className="logo">
+                <h1 className="logo__name">Dashboard</h1>
             </div>
-            <nav className='aside-nav'>
-                <NavLink className='aside-nav__link' to="/"><i className="fas fa-angle-right"></i>Home</NavLink>
-                <NavLink className='aside-nav__link' to="/feed"><i className="fas fa-angle-right"></i>Feed</NavLink>
-                <NavLink className='aside-nav__link' to="/notes"><i className="fas fa-angle-right"></i>Notes</NavLink>
-                <NavLink className='aside-nav__link' to="/profile"><i className="fas fa-angle-right"></i>Profile</NavLink>
-                <NavLink className='aside-nav__link' to="/saved"><i className="fas fa-angle-right"></i>Saved</NavLink>
+            <nav className="aside-nav">
+                <NavLink className="aside-nav__link" to="/">
+                    <i className={urlCheck()}></i>Home
+                </NavLink>
+                <NavLink className="aside-nav__link" to="/feed">
+                    <i className={urlCheck("#/feed")}></i>Feed
+                </NavLink>
+                <NavLink className="aside-nav__link" to="/notes">
+                    <i className={urlCheck("#/notes")}></i>Notes
+                </NavLink>
+                <NavLink className="aside-nav__link" to="/profile">
+                    <i className={urlCheck("#/profile")}></i>Profile
+                </NavLink>
+                <NavLink className="aside-nav__link" to="/saved">
+                    <i className={urlCheck("#/saved")}></i>Saved
+                </NavLink>
             </nav>
         </aside>
-    )
+    );
 };
 
 export default Aside;
